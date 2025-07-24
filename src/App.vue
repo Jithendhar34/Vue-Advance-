f99febe45baf6acf331c3975ca8d2bd0540899cb
KJithendhar <jithendharkampati04@gmail.com>
2025-07-24 23:11:56 +0530

    24

--- src/App.vue
+++ src/App.vue
@@ -2,47 +2,21 @@
     <div>
         <app-header/>
         <div class="container">
-            <Cars/>
-            <hr/>
-            <CarBrands>
-                <div>Content at the top</div>
-                <template v-slot:brands>
-                    <ul>
-                        <li v-for="(brand,index) in brands" :key="index">
-                            {{ brand }}
-                        </li>
-                    </ul>
-                </template>
-                <template v-slot:other>
-                    <div>Some other content</div>
-                </template>
-                <strong>Default slot</strong>
-            </CarBrands>
+            <button @click="activeComp = Mike">Mike</button>
+            <button @click="activeComp = Steve">Steve</button>
+            <!-- <Mike v-if="activeComp === Mike"/>
+            <Steve v-if="activeComp === Steve"/> -->
+            <component :is="activeComp"></component>
         </div>
     </div>
 </template>
 
 <script setup>
-    import { reactive, provide } from 'vue';
-    import Cars from './components/Cars/index.vue';
-    import CarBrands from './components/Cars/brands.vue';
+    import Mike from  './components/Players/Mike.vue'
+    import Steve from './components/Players/steve.vue';
+    import { shallowRef } from 'vue';
 
-    const brands = reactive(['Mazda','Honda','Renault'])
-    const cars = reactive([
-        {model:'F9',brand:'Ferrari'},
-        {model:'911',brand:'Porsche'},
-        {model:'Tipo',brand:'Fiat'}
-    ])
-    
-
-    const updateCar = () => {
-        cars[0].model = 'Ford'
-    }
-
-    provide('cars',{
-        cars,
-        updateCar
-    })
+    const activeComp = shallowRef(Mike);
 
 </script>
 
